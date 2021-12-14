75d831df030c3c7d83c13fb64c9cee40a8048621
thedon122 <donovan122@yahoo.com>
2021-12-14 16:20:00 +0100

    added two pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
 import react from "react";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return ( <div>PageOne</div>);
+};
+
+const PageTwo = () => {
+    return <div>PageTwo
+        <button>Click Me!</button>
+    </div>;
+};
 
 const App = ()  =>  {
     return <div>
-        App
+        <BrowserRouter>
+            <div>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pagetwo" exact component={PageTwo} />
+            </div>
+        </BrowserRouter>
     </div>
 };
 
