cf71b483a2ed847cf1d132419100f0d56bef707c
thedon122 <donovan122@yahoo.com>
2021-12-15 16:10:00 +0100

    react router link

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,11 @@
 import react from "react";
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
     return  ( 
                 <div>
                     PageOne
-                    BAD!!
-                    <a href="/pagetwo">Navigate to Page Two</a>
+                    <Link to="/pagetwo">Navigate to Page Two</Link>
                 </div>
             );
     };
@@ -16,8 +15,7 @@ const PageTwo = () => {
                 <div>
                     PageTwo
                     <button>Click Me!</button>
-                    BAD!!
-                    <a href="/">Navigate to Page One</a>
+                    <Link to="/">Navigate to Page One</Link>
                 </div>
             );
     };
