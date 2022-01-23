359a94247afb87f7057bbd0c54d7bd8fd217c5b9
thedon122 <donovan122@yahoo.com>
2022-01-23 11:01:00 +0100

    added form button submit

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -10,11 +10,18 @@ class StreamCreate extends React.Component {
         </div>
         );
     }
+
+    onSubmit(formValues) {
+        console.log(formValues);
+    }
     render (){
         return (
-        <form className="ui form">
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} 
+        className="ui form"
+        >
             <Field name="title" component={this.renderInput} label="Enter Title" />
             <Field name="Description" component={this.renderInput} label="Enter Description" />
+            <button className="ui button primary">Submit</button>
         </form>
         );
     }
