a2bd9cc921a9253c4e56dda0ba0fd5f3a13fea64
thedon122 <donovan122@yahoo.com>
2022-02-23 16:33:00 +0100

    added programmatic navigation for submit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import { formValues } from "redux-form";
 import streams from "../apis/streams.js";
+import history from '../history';
 import { SIGN_IN, 
     SIGN_OUT,
     CREATE_STREAM,
@@ -28,7 +29,7 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams',  { ...formValues, userId});
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
-    // Do some programmatic navigation 
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
