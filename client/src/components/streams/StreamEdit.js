d4c7d0194e70b5d916a7a776a90683627e9c833c
thedon122 <donovan122@yahoo.com>
2022-02-27 16:59:00 +0100

    added stream title to edit stream

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,6 +1,23 @@
 import React from "react";
-const StreamEdit = () => {
-    return (<div>StreamEdit</div>);
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    render() {
+        if(!this.props.stream) {
+            return <div>Loading...</div>
+        }
+
+        return <div>{this.props.stream.title}</div>
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    console.log(ownProps);
+    return { stream: state.streams[ownProps.match.params.id]};
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
