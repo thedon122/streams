2dbeeab6a9d0342c58b457dd90554bc844dbe8d8
thedon122 <donovan122@yahoo.com>
2022-03-07 00:52:00 +0100

    created modal added to streams delete

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Modal from "../Modal";
 const StreamDelete = () => {
-    return (<div>StreamDelete</div>);
+    return (<div>
+        StreamDelete
+        <Modal />
+        </div>);
 };
 
 export default StreamDelete;
