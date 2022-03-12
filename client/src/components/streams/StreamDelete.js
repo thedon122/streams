54011cffb6a71bf03803a87df6ba4ac6735fa64f
thedon122 <donovan122@yahoo.com>
2022-03-12 18:47:00 +0100

    added renderContent and mapStateToProps to delete

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -16,18 +16,30 @@ class StreamDelete extends React.Component {
             </React.Fragment>
         );
     }
+    renderContent() {
+        if(!this.props.stream) {
+            return 'Are you sure you want to delete this stream?'
+        }
+
+        return `Are you sure you want to delete this stream with title: ${this.props.stream.title}?`
+    }
     
     render() {
-        return (<div>
-            StreamDelete
+        return (
             <Modal 
                 title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
+                content={this.renderContent()}
                 actions={this.renderActions()}
                 onDismiss={() => history.push('/')}
             />
-            </div>);
+            );
     }
 }
 
-export default connect(null, {fetchStream})(StreamDelete);
+const mapStateToProps = (state ,ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+};
+
+export default connect(
+    mapStateToProps, 
+    { fetchStream })(StreamDelete);
